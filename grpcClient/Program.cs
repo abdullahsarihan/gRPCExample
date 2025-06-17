589605e89905b493402211e5a719c2acf22e89f3
abdullahsarihan <abdullah.sarihan.as@gmail.com>
2025-06-17 14:39:39 +0300

    first commit

---
+++ grpcClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Grpc.Net.Client;
+using grpcServer;
+
+namespace grpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5287");
+            var greetClient = new Greeter.GreeterClient(channel);
+
+            HelloReply result = await greetClient.SayHelloAsync(new HelloRequest
+            {
+                Name = "Abdullah'tan Selamlar"
+            });
+            System.Console.WriteLine(result.Message);
+        }
+    }
+}
